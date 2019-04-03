85463c860fc75a269d2d4f8996de533bcb130d0c
Khambay Phasouvor <khambayphasouvor.student@careerdevs.com>
2019-04-03 21:46:02 +0000

    created nestedArray and ready to push

---
+++ nestedArray.js
@@ -0,0 +1,20 @@
+// Nest one Array within Another Array.
+
+// Example
+var ourArray = [
+    ["the universe", 42],
+    ["everything", 101010]
+];
+
+var myArray = [
+    ['khammy', 31],
+    ['lucy', 32],
+    ['markus', 29]
+];
+
+// for loop to print out only the name in the newsted array
+for (var i = 0; i < myArray.length; i++) {
+    for (var j = 0; myArray.length[i]; j++) {
+    }
+    console.log("Hello Buddy:", myArray[i][j]);
+}
